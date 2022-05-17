1f15e5dae2ec421261fe86cf48cd1e58161ce735
Srushti <srushti208shetye@gmail.com>
2022-05-17 11:08:44 +0530

    added pipe, directive and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+git config --global user.nameimport { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
